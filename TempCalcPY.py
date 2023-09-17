7dffb52955347b7fc504b60da854aeb6fb8c4cb6
asimtiaz77 <142457766+asimtiaz77@users.noreply.github.com>
2023-09-16 22:16:49 -0400

    Add files via upload

---
+++ TempCalcPY.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+# input temperature 
+temperature = input("Enter the patient's temperature (in degrees Fahrenheight): ")
+
+
+# In[3]:
+
+
+# check temperature 
+if temperature.replace(".", "", 1).isdigit():
+    temperature_input = float(temperature)
+    
+    if temperature_input >= 99.5:
+        print("The patient has a high temperature.") 
+    else:
+        print("The patient has a normal temperature.") 
+            
+else: 
+    print("Error: Please enter a valid temperature (numeric value).")
+        
+            
+            
+
+
+# In[4]:
+
+
+# input temperature 
+temperature = input("Enter the patient's temperature (in degrees Fahrenheight): ")
+
+
+# In[6]:
+
+
+# check temperature 
+if temperature.replace(".", "", 1).isdigit():
+    temperature_input = float(temperature)
+    
+    if temperature_input >= 99.5:
+        print("The patient has a high temperature.") 
+    else:
+        print("The patient has a normal temperature.") 
+            
+else: 
+    print("Error: Please enter a valid temperature (numeric value).")
+        
+            
+
+
+# In[7]:
+
+
+# input temperature 
+temperature = input("Enter the patient's temperature (in degrees Fahrenheight): ")
+
+
+# In[8]:
+
+
+# check temperature 
+if temperature.replace(".", "", 1).isdigit():
+    temperature_input = float(temperature)
+    
+    if temperature_input >= 99.5:
+        print("The patient has a high temperature.") 
+    else:
+        print("The patient has a normal temperature.") 
+            
+else: 
+    print("Error: Please enter a valid temperature (numeric value).")
+        
+
+
+# In[9]:
+
+
+# input temperature 
+temperature = input("Enter the patient's temperature (in degrees Fahrenheight): ")
+
+
+# In[10]:
+
+
+# check temperature 
+if temperature.replace(".", "", 1).isdigit():
+    temperature_input = float(temperature)
+    
+    if temperature_input >= 99.5:
+        print("The patient has a high temperature.") 
+    else:
+        print("The patient has a normal temperature.") 
+            
+else: 
+    print("Error: Please enter a valid temperature (numeric value).")
+        
+
+
+# In[ ]:
+
+
+
+
